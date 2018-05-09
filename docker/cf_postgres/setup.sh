ff7adb249674c44d0f717e0b55ec64a53ecd7e80
amb1ent <ben+dev@amb1ent.org>
2018-05-09 11:35:40 +0100

    initialise databases with two schema "db" for db-version-management and "cf" for core-fabric tables

--- docker/cf_postgres/setup.sh
+++ docker/cf_postgres/setup.sh
@@ -4,12 +4,40 @@
 
 service postgresql start
 
-su - postgres -c psql <<SQL
+su - postgres -c psql <<PSQL
 ALTER ROLE postgres WITH SUPERUSER LOGIN PASSWORD 'password';
 
 CREATE DATABASE corefabric__node_db;
 
 CREATE DATABASE corefabric__config_db;
-SQL
+
+\connect corefabric__node_db
+
+CREATE SCHEMA db;
+CREATE SCHEMA cf;
+
+CREATE TABLE db.schema_versions (
+    schema_version_id       bigserial       NOT NULL,
+    schema_tag              varchar(64)     NOT NULL DEFAULT 'node',
+    schema_revision         bigint          NOT NULL,
+    CONSTRAINT pk PRIMARY KEY (schema_version_id),
+    CONSTRAINT tag_revision UNIQUE (schema_tag, schema_revision)
+);
+INSERT INTO db.schema_versions (schema_tag, schema_revision) VALUES ('node', 1);
+
+\connect corefabric__config_db;
+
+CREATE SCHEMA db;
+CREATE SCHEMA cf;
+
+CREATE TABLE db.schema_versions (
+    schema_version_id       bigserial       NOT NULL,
+    schema_tag              varchar(64)     NOT NULL DEFAULT 'config',
+    schema_revision         bigint          NOT NULL,
+    CONSTRAINT pk PRIMARY KEY (schema_version_id),
+    CONSTRAINT tag_revision UNIQUE (schema_tag, schema_revision)
+);
+INSERT INTO db.schema_versions (schema_tag, schema_revision) VALUES ('config', 1);
+PSQL
 
 service postgresql stop
