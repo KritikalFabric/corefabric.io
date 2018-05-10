b1ef4cfb880740f39441dabc1573147a31077911
amb1ent <ben+dev@amb1ent.org>
2018-05-10 18:12:43 +0100

    don't alway clean before build

--- docker-nodejs-clean.sh
+++ docker-nodejs-clean.sh
@@ -8,4 +8,4 @@ docker pull $DOCKER_REPO/cf_nodejs
 docker tag $DOCKER_REPO/cf_base corefabric:cf_base
 docker tag $DOCKER_REPO/cf_nodejs corefabric:cf_nodejs
 
-docker run -v `pwd`/a2:/a2 corefabric:cf_nodejs /bin/bash -c 'cd /a2; ./build.sh'
+docker run -v `pwd`/a2:/a2 corefabric:cf_nodejs /bin/bash -c 'cd /a2; ./clean.sh'
