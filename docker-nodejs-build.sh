6016f11b49f09a92311dd452af5dad750e4194f5
amb1ent <ben+dev@amb1ent.org>
2018-05-10 16:08:36 +0100

    nodejs 10.x container to run the angular development environment, see #1

--- docker-nodejs-build.sh
+++ docker-nodejs-build.sh
@@ -2,16 +2,10 @@
 
 export DOCKER_REPO=registry.dc1.amb1ent.org:443
 
-docker-compose stop || true
-docker-compose rm -f || true
-
 docker pull $DOCKER_REPO/cf_base
 docker pull $DOCKER_REPO/cf_nodejs
-docker pull $DOCKER_REPO/cf_postgres
 
 docker tag $DOCKER_REPO/cf_base corefabric:cf_base
 docker tag $DOCKER_REPO/cf_nodejs corefabric:cf_nodejs
-docker tag $DOCKER_REPO/cf_postgres corefabric:cf_postgres
 
-docker-compose up
-
+docker run -v `pwd`/a2:/a2 corefabric:cf_nodejs /bin/bash -c 'cd /a2; ./clean.sh; ./build.sh'
--- docker-publish-amb1ent.sh
+++ docker-publish-amb1ent.sh
@@ -6,6 +6,10 @@ docker build --no-cache docker/cf_base --tag corefabric:cf_base && \
 docker tag corefabric:cf_base $DOCKER_REPO/cf_base && \
 docker push $DOCKER_REPO/cf_base && \
 
+docker build --no-cache docker/cf_nodejs --tag corefabric:cf_nodejs && \
+docker tag corefabric:cf_nodejs $DOCKER_REPO/cf_nodejs && \
+docker push $DOCKER_REPO/cf_nodejs && \
+
 docker build --no-cache docker/cf_postgres --tag corefabric:cf_postgres && \
 docker tag corefabric:cf_postgres $DOCKER_REPO/cf_postgres && \
 docker push $DOCKER_REPO/cf_postgres && \
