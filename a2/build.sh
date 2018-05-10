5a8b6b6c540a5459ac312ae7aa753eeead4c94ac
amb1ent <ben+dev@amb1ent.org>
2018-05-10 19:42:29 +0100

    fix build, lite-server @ compatible version

--- a2/build.sh
+++ a2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 npm install typings
-npm install lite-server
+npm install lite-server@2.2.2
 npm install && npm run typings install && npm run tsc # && npm run gulp
--- a2/run.sh
+++ a2/run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+npm install typings
+npm install lite-server@2.2.2
 npm install && npm start
