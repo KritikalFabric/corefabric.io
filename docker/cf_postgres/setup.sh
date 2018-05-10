0c656dfe338af2a13ec79f79c1d454de0575413b
amb1ent <ben+dev@amb1ent.org>
2018-05-10 19:06:15 +0100

    fix schema

--- docker/cf_postgres/setup.sh
+++ docker/cf_postgres/setup.sh
@@ -22,15 +22,15 @@ CREATE TABLE db.schema_versions (
     schema_version_id       bigserial       NOT NULL,
     schema_tag              varchar(64)     NOT NULL DEFAULT 'node',
     schema_revision         bigint          NOT NULL,
-    CONSTRAINT pk PRIMARY KEY (schema_version_id),
-    CONSTRAINT tag_revision UNIQUE (schema_tag, schema_revision)
+    CONSTRAINT db__pk__schema_versions                  PRIMARY KEY (schema_version_id),
+    CONSTRAINT db__tag_revision__schema_versions        UNIQUE (schema_tag, schema_revision)
 );
 INSERT INTO db.schema_versions (schema_tag, schema_revision) VALUES ('node', 1);
 
 CREATE TABLE node.instance_local_cfg (
     key                     varchar(64)     NOT NULL,
     value                   text            NOT NULL,
-    CONSTRAINT pk PRIMARY KEY (key)
+    CONSTRAINT node__pk__instance_local_cfg PRIMARY KEY (key)
 );
 
 CREATE TABLE node.send_q (
@@ -38,12 +38,12 @@ CREATE TABLE node.send_q (
     a                       text            NOT NULL,
     b                       jsonb           NOT NULL,
     dt                      timestamptz     NOT NULL,
-    CONSTRAINT pk PRIMARY KEY (q_id),
-    CONSTRAINT dt UNIQUE (dt, q_id)
+    CONSTRAINT node__pk__send_q                          PRIMARY KEY (q_id),
+    CONSTRAINT node__dt__send_q                          UNIQUE (dt, q_id)
 );
 
 CREATE OR REPLACE FUNCTION node.dequeue_send_q(addresses text[]) RETURNS SETOF node.send_q AS \$\$
-DECLARE c CURSOR FOR SELECT * FROM node.send_q WHERE ARRAY[a] <@ addresses AND dt < current_timestamp FOR UPDATE OF send_q;
+DECLARE c CURSOR FOR SELECT * FROM node.send_q WHERE ARRAY[a] <@ addresses AND dt < current_timestamp FOR UPDATE OF node.send_q;
 BEGIN
 FOR r IN c LOOP
     DELETE FROM node.send_q WHERE CURRENT OF c;
@@ -63,8 +63,8 @@ CREATE TABLE db.schema_versions (
     schema_version_id       bigserial       NOT NULL,
     schema_tag              varchar(64)     NOT NULL DEFAULT 'config',
     schema_revision         bigint          NOT NULL,
-    CONSTRAINT pk PRIMARY KEY (schema_version_id),
-    CONSTRAINT tag_revision UNIQUE (schema_tag, schema_revision)
+    CONSTRAINT db__pk__schema_versions                  PRIMARY KEY (schema_version_id),
+    CONSTRAINT db__tag_revision__schema_versions        UNIQUE (schema_tag, schema_revision)
 );
 INSERT INTO db.schema_versions (schema_tag, schema_revision) VALUES ('config', 1);
 PSQL
