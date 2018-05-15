4c024afc8da749d043c34d6783951ee715996ee7
amb1ent <ben+dev@amb1ent.org>
2018-05-15 09:57:58 +0100

    HTTP headers: Accept-Encoding and Content-Encoding set, statically gzipped static content served.  see #20

--- a2/build.sh
+++ a2/build.sh
@@ -2,5 +2,5 @@
 
 lessc src/download/maverick-theme/app/assets/less/styles.less src/download/maverick-theme/app/assets/less/maverick-theme.css
 ng build --prod --build-optimizer --common-chunk
-find a2/dist/a2/ -type f -not -name '*.gz' -exec gzip --best --keep {} \;
+find dist/a2/ -type f -not -name '*.gz' -exec gzip --best --keep {} \;
 echo "Build completed."
