5f87579a9cd77a688d2cf910be6b350a7f997b91
amb1ent <ben+dev@amb1ent.org>
2020-02-04 09:58:54 +0000

    mail text deformatting -- into corefabric with tests -->

---
+++ src/main/java/org/kritikal/fabric/net/smtp/Rfc822PlainReplyFormats.java
@@ -0,0 +1,64 @@
+package org.kritikal.fabric.net.smtp;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Rfc822PlainReplyFormats {
+
+    private static Pattern reOnWrote = Pattern.compile("^\\s*on\\s+.*\\s+wrote:\\s*$", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+    private static Pattern reVendors = Pattern.compile("^\\s*(Get Outlook for|Sent from Yahoo)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+
+    public static String scrub(String emailBody) {
+        return stripSignature2(
+            stripSignature1(
+                stripQuotedContent(emailBody).trim()
+            ).trim()
+        ).trim();
+    }
+
+    private static String stripQuotedContent(String emailBody) {
+        int quote = emailBody.indexOf("\n> ");
+        int outlook = emailBody.indexOf('\ufeff', 4);
+        int lookout = emailBody.indexOf("\n____");
+        int ascii = emailBody.indexOf("\n----");
+
+        if (quote > -1 && outlook > 0 && quote > outlook - 1) {
+            quote = outlook - 1;
+        }
+        if (quote > -1) {
+            return emailBody.substring(0, quote + 1);
+        } else if (outlook > -1) {
+            return emailBody.substring(0, outlook);
+        } else if (lookout > -1) {
+            return emailBody.substring(0, lookout + 1);
+        } else if (ascii > -1) {
+            return emailBody.substring(0, ascii + 1);
+        }
+        return emailBody;
+    }
+
+    private static String stripSignature1(String emailBody) {
+        int signature = emailBody.indexOf("\n--");
+        if (signature > -1) {
+            return emailBody.substring(0, signature+1);
+        }
+        return emailBody;
+    }
+
+    private static String stripSignature2(String emailBody) {
+        Matcher mOnWrote = reOnWrote.matcher(emailBody);
+        boolean foundOnWrote = mOnWrote.find();
+        Matcher mVendors = reVendors.matcher(emailBody);
+        boolean foundVendors = mVendors.find();
+        if (foundOnWrote && foundVendors) {
+            int i = Math.min(mOnWrote.start(), mVendors.start());
+            return emailBody.substring(0, i);
+        } else if (foundOnWrote) {
+            return emailBody.substring(0, mOnWrote.start());
+        } else if (foundVendors) {
+            return emailBody.substring(0, mVendors.start());
+        }
+        return emailBody;
+    }
+
+}
---
+++ src/test/java/org/kritikal/fabric/core/NR001_Rfc822_Test.java
@@ -0,0 +1,128 @@
+package org.kritikal.fabric.core;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kritikal.fabric.net.smtp.Rfc822PlainReplyFormats;
+import org.kritikal.platform.MyVertxUnitRunner;
+
+@RunWith(MyVertxUnitRunner.class)
+public class NR001_Rfc822_Test {
+
+    @Test
+    public void nr001_001_apple_mail() {
+        Assert.assertEquals(
+                "Test mail",
+                Rfc822PlainReplyFormats.scrub("Test mail\n\n> quoted text\n> second line\n")
+                );
+    }
+
+    @Test
+    public void nr001_002_outlook_desktop() {
+        Assert.assertEquals(
+                "Test mail",
+                Rfc822PlainReplyFormats.scrub("Test mail\n\n\ufeffQuoted text starts here in desktop outlook\n")
+                );
+    }
+
+    @Test
+    public void nr001_003_outlook_mobile() {
+        Assert.assertEquals(
+                "Test mail",
+                Rfc822PlainReplyFormats.scrub("Test mail\n\n____\nQuoted text starts here in mobile outlook\n")
+                );
+    }
+
+    @Test
+    public void nr001_004_thunderbird_desktop() {
+        Assert.assertEquals(
+                "reply from thunderbird/yahoo\n\ndid it work?",
+                Rfc822PlainReplyFormats.scrub("reply from thunderbird/yahoo\n" +
+                        "\n" +
+                        "did it work?\n" +
+                        "\n" +
+                        "On 03/02/2020 19:47, street-stall.space - planet ferenginar wrote:\n" +
+                        "> First reply Yahoo mail mobile\n" +
+                        ">\n" +
+                        "> Sent from Yahoo Mail on Android\n" +
+                        ">   \n" +
+                        ">    On Mon, 3 Feb 2020 at 20:46, street-stall.space - planet ferenginar<please-reply@ferenginar.street-stall.space> wrote:   First reply sysop\n" +
+                        ">")
+        );
+    }
+
+    @Test
+    public void nr001_005_yahoo_mobile() {
+        Assert.assertEquals(
+                "First reply Yahoo mail mobile",
+                Rfc822PlainReplyFormats.scrub("First reply Yahoo mail mobile\n" +
+                        "\n" +
+                        "Sent from Yahoo Mail on Android\n" +
+                        "\n" +
+                        "    On Mon, 3 Feb 2020 at 20:46, street-stall.space - planet ferenginar\n" +
+                        "    <please-reply@ferenginar.street-stall.space> wrote:\n" +
+                        "    First reply sysop\n" +
+                        "\n" +
+                        "    -- \n" +
+                        "    sysop bbs    street-stall.space - planet ferenginar\n" +
+                        "    https://ferenginar.street-stall.space/-/bbs/post/c1217e83-6c2e-4387-a036-0c38fc1f3106/ac4af582-dea8-4ddf-8439-4675b29389d4\n")
+        );
+    }
+
+    @Test
+    public void nr001_006_outlook_mobile() {
+        Assert.assertEquals(
+                "Some tickets never close",
+                Rfc822PlainReplyFormats.scrub(
+                "Some tickets never close\n" +
+                       "\n" +
+                       "Get Outlook for Android<https://aka.ms/ghei36>\n" +
+                       "\n" +
+                       "________________________________\n" +
+                       "From: street-stall.space - planet ferenginar <please-reply@ferenginar.street-stall.space>\n" +
+                       "Sent: Monday, February 3, 2020 8:06:19 PM\n" +
+                       "To: street-stall-sysop@hotmail.com <street-stall-sysop@hotmail.com>\n" +
+                       "Subject: Re: test post please reply from outlook eventually\n" +
+                       "\n" +
+                       "Yes please close the ticket!"
+                )
+        );
+    }
+
+    @Test
+    public void nr001_007_thunderbird_desktop_forward() {
+        Assert.assertEquals(
+                "Your response",
+                Rfc822PlainReplyFormats.scrub("Your response\n" +
+                        "\n" +
+                        "\n" +
+                        "-------- Forwarded Message --------\n" +
+                        "Date: \tMon, 3 Feb 2020 19:47:17 +0000 (UTC)\n" +
+                        "From: \tBen Gould <street_stall_sysop@yahoo.com>\n" +
+                        "Reply-To: \tstreet_stall_sysop@yahoo.com <street_stall_sysop@yahoo.com>\n" +
+                        "To: \treply.ac4af582-dea8-4ddf-8439-4675b29389d4.bbs-post@ferenginar.street-stall.space <reply.ac4af582-dea8-4ddf-8439-4675b29389d4.bbs-post@ferenginar.street-stall.space>\n" +
+                        "Message-ID: \t<1208689595.1957749.1580759237959@mail.yahoo.com>\n" +
+                        "In-Reply-To: \t<5aa490ef-a265-4abc-b77f-adde549079d1@ferenginar.street-stall.space>\n" +
+                        "References: \t<5aa490ef-a265-4abc-b77f-adde549079d1@ferenginar.street-stall.space>\n" +
+                        "Subject: \tRe: Test Yahoo mail\n" +
+                        "MIME-Version: \t1.0\n" +
+                        "Content-Type: \tmultipart/alternative; boundary=\"----=_Part_1957748_482627109.1580759237957\"\n" +
+                        "X-Mailer: \tWebService/1.1.15149 YahooMailAndroidMobile YMobile/1.0 (com.yahoo.mobile.client.android.mail/6.2.4; Android/9; HUAWEIANE-L21; HWANE; HUAWEI; ANE-LX1; 5.37; 2060x1080;)\n" +
+                        "Content-Length: \t1999\n" +
+                        "\n" +
+                        "\n" +
+                        "First reply Yahoo mail mobile\n" +
+                        "\n" +
+                        "Sent from Yahoo Mail on Android\n" +
+                        "\n" +
+                        "    On Mon, 3 Feb 2020 at 20:46, street-stall.space - planet ferenginar\n" +
+                        "    <please-reply@ferenginar.street-stall.space> wrote:\n" +
+                        "    First reply sysop\n" +
+                        "\n" +
+                        "    -- \n" +
+                        "    sysop bbs    street-stall.space - planet ferenginar\n" +
+                        "    https://ferenginar.street-stall.space/-/bbs/post/c1217e83-6c2e-4387-a036-0c38fc1f3106/ac4af582-dea8-4ddf-8439-4675b29389d4\n" +
+                        "\n")
+        );
+    }
+}
